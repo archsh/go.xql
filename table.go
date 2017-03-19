b1d66e5e052596e21568fb4055568dbae6f93c1d
Mingcai SHEN <archsh@gmail.com>
2017-03-19 14:09:28 +0800

    Updated: support embedded struct for table declaration.

--- table.go
+++ table.go
@@ -1,6 +1,7 @@
 package xql
 
 import (
+    //"fmt"
     "reflect"
     "strings"
 )
@@ -26,23 +27,26 @@ type Column struct {
     PrimaryKey bool
 }
 
-func DeclareTable(name string, entity interface{}, schema ...string) *Table {
-
-    t := &Table{
-        TableName:name,
-        Entity: entity,
-        Columns: make(map[string]Column),
-    }
-    if len(schema) > 0 {
-        t.Schema = schema[0]
-    }
+func _make_columns(entity interface{}) (cols []Column) {
     if nil != entity {
         et := reflect.TypeOf(entity)
+        ev := reflect.ValueOf(entity)
         for i:=0; i< et.Elem().NumField(); i++ {
             f := et.Elem().Field(i)
             //f.Name
-            c := Column{PropertyName:f.Name}
             x_tags := strings.Split(f.Tag.Get("xql"),",")
+            if f.Anonymous  {
+                //fmt.Println("_make_columns:>", f.Name, "is Anonymous!", ev.Elem().Field(i))
+                if x_tags[0] != "-" {
+                    for _, c := range _make_columns(ev.Elem().Field(i).Addr().Interface()) {
+                        cols = append(cols, c)
+                    }
+                }else{
+                    continue
+                }
+                continue
+            }
+            c := Column{PropertyName:f.Name}
             if len(x_tags) < 1 || x_tags[0]=="" {
                 c.FieldName = Camel2Underscore(f.Name)
             }else if x_tags[0] == "-" {
@@ -53,7 +57,7 @@ func DeclareTable(name string, entity interface{}, schema ...string) *Table {
                     switch x {
                     case "pk":
                         c.PrimaryKey = true
-                        t.PrimaryKey = append(t.PrimaryKey, x)
+                        //t.PrimaryKey = append(t.PrimaryKey, x)
                     case "indexed":
                         c.Indexed = true
                     case "nullable":
@@ -65,7 +69,29 @@ func DeclareTable(name string, entity interface{}, schema ...string) *Table {
                     }
                 }
             }
+            cols = append(cols, c)
+            //t.Columns[c.FieldName] = c
+        }
+    }
+    return cols
+}
+
+func DeclareTable(name string, entity interface{}, schema ...string) *Table {
+
+    t := &Table{
+        TableName:name,
+        Entity: entity,
+        Columns: make(map[string]Column),
+    }
+    if len(schema) > 0 {
+        t.Schema = schema[0]
+    }
+    if nil != entity {
+        for _, c := range _make_columns(entity) {
             t.Columns[c.FieldName] = c
+            if c.PrimaryKey {
+                t.PrimaryKey = append(t.PrimaryKey, c.FieldName)
+            }
         }
     }
     return t
