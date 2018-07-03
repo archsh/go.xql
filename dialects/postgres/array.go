fc1c57ed16f80bc5c45ac5490f90a323d423eba2
Mingcai SHEN <archsh@gmail.com>
2018-07-03 19:19:49 +0800

    Improved StringArray.

--- dialects/postgres/array.go
+++ dialects/postgres/array.go
@@ -23,7 +23,11 @@ func (h StringArray) Declare(props xql.PropertySet) string {
 }
 
 func (a StringArray) Elem2Strings() []string {
-    return a
+    var ss []string
+    for _, x := range a {
+        ss = append(ss, fmt.Sprintf("'%s'", strings.Replace(x, "'","\\'",-1)))
+    }
+    return ss
 }
 
 func (a *StringArray) Strings2Elem(ss ...string) error {
