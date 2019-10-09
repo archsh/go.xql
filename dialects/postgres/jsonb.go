8a8c1208b919524a99cf57f31b6586287b06c278
Mingcai SHEN <archsh@gmail.com>
2019-10-09 10:00:39 +0800

    Updated: Change to use pq.Array

--- dialects/postgres/jsonb.go
+++ dialects/postgres/jsonb.go
@@ -1,62 +1,62 @@
 package postgres
 
 import (
-    "errors"
-    "database/sql/driver"
-    "encoding/json"
-    "github.com/archsh/go.xql"
+	"database/sql/driver"
+	"encoding/json"
+	"errors"
+
+	"github.com/archsh/go.xql"
 )
 
 type JSON struct {
-
 }
 
 func (j JSON) Declare(props xql.PropertySet) string {
-    return "json"
+	return "json"
 }
 
 func (j *JSON) Scan(value interface{}) error {
-    return JSONB_Scan(j, value)
+	return JSONB_Scan(j, value)
 }
 
 func (j JSON) Value() (driver.Value, error) {
-    return JSONB_Value(j)
+	return JSONB_Value(j)
 }
 
-type JSONB JSON 
+type JSONB JSON
 
 func (j JSONB) Declare(props xql.PropertySet) string {
-    return "JSONB"
+	return "JSONB"
 }
 
 func (j *JSONB) Scan(value interface{}) error {
-    return JSONB_Scan(j, value)
+	return JSONB_Scan(j, value)
 }
 
 func (j JSONB) Value() (driver.Value, error) {
-    return JSONB_Value(j)
+	return JSONB_Value(j)
 }
 
 func JSONB_Scan(dest interface{}, src interface{}) error {
-    if nil == src {
-        dest = nil
-        return nil
-    }
-    source, ok := src.([]byte)
-    if !ok {
-        return errors.New("Type assertion .([]byte) failed.")
-    }
-    //entityType := reflect.TypeOf(dest)
-    //obj := reflect.New(entityType)
-    err := json.Unmarshal(source, dest)
-    if err != nil {
-        return err
-    }
-    //reflect.Indirect(dest).Set(obj)
-    //dest = obj.Elem().Addr().Interface()
-    return nil
+	if nil == src {
+		dest = nil
+		return nil
+	}
+	source, ok := src.([]byte)
+	if !ok {
+		return errors.New("Type assertion .([]byte) failed.")
+	}
+	//entityType := reflect.TypeOf(dest)
+	//obj := reflect.New(entityType)
+	err := json.Unmarshal(source, dest)
+	if err != nil {
+		return err
+	}
+	//reflect.Indirect(dest).Set(obj)
+	//dest = obj.Elem().Addr().Interface()
+	return nil
 }
 
 func JSONB_Value(obj interface{}) (driver.Value, error) {
-    return json.Marshal(obj)
+	return json.Marshal(obj)
 }
